a76752f0b624dfe191d8f8350ee49dbd70758dce
Alexandros Leontakianakos <alleon20@gmail.com>
2017-06-22 13:38:37 +0300

    Initial Commit with basic setup

---
+++ js/master.js
@@ -0,0 +1,24 @@
+/*------------------------
+    Global Variables    */
+
+
+
+
+/*----------------
+    Basic setup */
+function setup() {
+    createCanvas(600,400);
+}
+
+
+
+/*----------------
+    Game loop   */
+function draw() {
+    background(51);
+}
+
+
+/*----------------------------------------
+    End, rest of code on other files
+----------------------------------------*/
